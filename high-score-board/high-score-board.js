43e0767fd5baa6b362ac9ba7d5ad085c3ff6ad3c
Jorge Calleja <posicionadoenlaweb@gmail.com>
2024-02-17 23:18:37 +0100

    delete operator, loops, shorthand assignment operator, objects

--- high-score-board/high-score-board.js
+++ high-score-board/high-score-board.js
@@ -15,20 +15,11 @@ export function createScoreBoard() {
  */
 
 
-const scoreBoard = {
-  'Fiorella Ordaz': 2000,
-  'Jorge Calleja': 1000,
-}
-
 export function addPlayer(scoreBoard, player, score) {
   scoreBoard[player] = score;
-  console.log(scoreBoard[player]);
-  console.log(player);
   return scoreBoard
 }
-// console.log(scoreBoard);
-// console.log(addPlayer(scoreBoard, 'Jhon Macias', 1337));
-// addPlayer(scoreBoard, 'Jhon Macias', 1337)
+
 
 /**
  * Removes a player from a score board.
@@ -42,12 +33,11 @@ export function addPlayer(scoreBoard, player, score) {
 // Use the delete operator.
 // Reference the key like you have done in the task before (bracket notation).
 
-// function removePlayer(scoreBoard, player) {
-//   delete scoreBoard[player]
-//   return scoreBoard
-// }
+export function removePlayer(scoreBoard, player) {
+  delete scoreBoard[player]
+  return scoreBoard
+}
 
-// console.log(removePlayer(scoreBoard, 'Jesse Johnson'));
 
 /**
  * Increases a player's score by the given amount.
@@ -61,12 +51,9 @@ export function addPlayer(scoreBoard, player, score) {
 
 export function updateScore(scoreBoard, player, points) {
   scoreBoard[player] += points
-  console.log(scoreBoard[player]);
   return scoreBoard
 }
 
-// console.log(updateScore(scoreBoard, 'Jhon Macias', 3000));
-// console.log(scoreBoard);
 
 /**
  * Applies 100 bonus points to all players on the board.
@@ -78,11 +65,10 @@ export function updateScore(scoreBoard, player, points) {
 export function applyMondayBonus(scoreBoard) {
   for (let score in scoreBoard) {
     scoreBoard[score] += 100
-    console.log(scoreBoard);
   }
   return scoreBoard
 }
-// console.log(applyMondayBonus(scoreBoard));
+
 
 /**
  * Normalizes a score with the provided normalization function.
@@ -92,17 +78,7 @@ export function applyMondayBonus(scoreBoard) {
  */
 
 
-// const params = {
-//   score: 2000,
-//   normalizeFunction: normalize
-// }
-
-// function normalize(score) {
-//   console.log(params.score);
-//   return 2 * score + 10;
-// }
-
-function normalizeScore(params) {
+export function normalizeScore(params) {
   console.log({ params });
   return params.normalizeFunction(params.score)
 }

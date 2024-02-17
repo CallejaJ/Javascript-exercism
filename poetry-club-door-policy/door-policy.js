0c0b40238a29f0f00345bc993aee52167ab77811
Jorge Calleja <posicionadoenlaweb@gmail.com>
2024-02-18 00:16:36 +0100

    string methods, string properties

--- poetry-club-door-policy/door-policy.js
+++ poetry-club-door-policy/door-policy.js
@@ -1,23 +1,5 @@
 // @ts-check
 //
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-//
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./door-policy.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Good luck with that door policy!
 
 /**
  * Respond with the correct character, given the line of the
@@ -55,7 +37,6 @@ export function frontDoorPassword(word) {
 export function backDoorResponse(line) {
   const sentence = line.replace(/ /g, "").trim() // le quita los 5 espacios en blanco
   return sentence.at(-1);
-  return letter
 }
 
 /**
